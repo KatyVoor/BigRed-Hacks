8cdf8908ac2c22aaa974c3c2ab01d4dc332455aa
Nandita Mohan <nkm39@cornell.edu>
2017-09-16 18:29:06 -0400

    Add files via upload
    Added vertical and horizontal lines - must be tested

--- brh-app/src/components/DrawLines.js
+++ brh-app/src/components/DrawLines.js
@@ -13,23 +13,32 @@ const DrawLines = {
 
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
+    var con1 = c.getContext("2d"); //horizontal
+    var con2 = c.getContext("2d"); //vertical
 
     var centerX = 0;
     var centerY = 0;
     var radius = minorAxis;
     ctx.save()
 
-    ctx.translate(c.width / 2, c.height / 2);
-
+    ctx.translate(minorAxis, majorAxis;
+    con1.translate(minorAxis*2,majorAxis);
+    con2.translate(minorAxis, majorAxis*2);
     // scale context vertically
-    ctx.scale(minorAxis/majorAxis, 1);
+    ctx.scale((minorAxis*2)/(majorAxis*2), 1);
 
     // draw circle which will be stretched into an oval
     ctx.beginPath();
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-
+    con1.beginPath();
+    con1.lineTo(0-(minorAxis*2), 0);
+    con2.lineTo(0, 0-(majorAxis*2));
     // restore to original state
     ctx.restore();
+    con1.restore();
+    con2.restore();
 
     ctx.stroke();
+    con1.stroke();
+    con2.stroke();
 }
